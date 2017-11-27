73791913969b32d9db10326add08b1fe84025325
Moss Uchida <super@192.168.0.13>
2017-11-27 18:34:26 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,34 +1,11 @@
-# Acme Air Sample and Benchmark (wasperf version)
+# Acme Air Sample and Benchmark (monolithic simple version)
 
 This application shows an implementation of a fictitious airline called "Acme Air".  The application was built with some key business requirements: the ability to scale to billions of web API calls per day, the need to develop and deploy the application targeting multiple cloud platforms (including Public, Dedicated, Private and hybrid) and the need to support multiple channels for user interaction (with mobile enablement first and browser/Web 2.0 second).The application can be deployed both on-prem as well as on Cloud platforms. 
-
-This application has been restructured to be a microservices application in addion to a monolithic application. Monolithc appliction is refactored to make sure the application can scale with multiple Cloud Data Servces. Microservices version is being developed actively to support multiple Service Proxy, Service Discovery and Resliency technologies. This application is also being enhanced with additional services using IBM Cognitive capabilites like Watson Dialog etc.,
 
 This version of acmeair supports:
   - WebSphere Liberty Profile to Mongodb
 
-## Repository Contents
-
-Source:
-
-- **acmeair-services**:  The Java data services interface definitions
-- **acmeair-service-mongo**:  A WebSphere eXtreme Scale data service implementation
-- **acmeair-webapp**:  The Web 2.0 application and associated Java REST services (monolithic app)
-- **acmeair-mainapp**:  The Front End and Config MicroService application
-- **acmeair-as**:  The Authentication Java Rest MicroService application
-- **acmeair-cs**:  The Customer Java Rest MicroService application
-- **acmeair-fs**:  The Flight Java Rest MicroService application
-- **acmeair-bs**:  The Booking Java Rest MicroService application
-
-## How to get started
-
-* Instructions for [setting up and building the codebase](Documentation/Build_Instructions.md)
-* Deploying the sample application to [Websphere Liberty](Documentation/Liberty_Instructions.md)
-* Deploying the sample application as microservices in docker [Websphere Liberty](Documentation/Docker_Instructions.md)
-* Deploying to [IBM Bluemix](Documentation/Bluemix_Instructions.md)
-* Acme Air for Node.js [Instructions](https://github.com/wasperf/acmeair-nodejs/blob/master/README.md)
-
-* NOTE: DO NOT use acmeair.properties file to configure database unless there is specific needs.  Use Service Bridge for Mongo DB to get good performance results (When using acmeair.properties file, make sure to configure every DB options properly - if only setting up the hostname, port number & credentials, it will give poor performance)
-
-
+#Setup
+Use maven to build the project
+ mvn clean package
 
---
+++ src/main/java/com/acmeair/mongo/ConnectionManager.java
@@ -0,0 +1,120 @@
+package com.acmeair.mongo;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Logger;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import com.acmeair.mongo.MongoConstants;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+
+public class ConnectionManager implements MongoConstants {
+
+	private static AtomicReference<ConnectionManager> connectionManager = new AtomicReference<ConnectionManager>();
+
+	private final static Logger logger = Logger.getLogger(ConnectionManager.class.getName());
+
+	protected MongoClient mongoClient;
+	protected MongoDatabase db;
+
+	public static ConnectionManager getConnectionManager() {
+		if (connectionManager.get() == null) {
+			synchronized (connectionManager) {
+				if (connectionManager.get() == null) {
+					connectionManager.set(new ConnectionManager());
+				}
+			}
+		}
+		return connectionManager.get();
+	}
+
+	private ConnectionManager() {
+
+		// Set default client options, and then check if there is a properties
+		// file.
+		String hostname = "localhost";
+		int port = 27017;
+		String dbname = "acmeair";
+
+		String mongoManual = System.getenv("MONGO_MANUAL");
+		Boolean isManual = Boolean.parseBoolean(mongoManual);
+
+		String mongoHost = System.getenv("MONGO_HOST");
+		if (mongoHost != null) {
+			hostname = mongoHost;
+		}
+
+		String mongoPort = System.getenv("MONGO_PORT");
+		if (mongoPort != null) {
+			port = Integer.parseInt(mongoPort);
+		}
+
+		String mongoDbName = System.getenv("MONGO_DBNAME");
+		if (mongoDbName != null) {
+			dbname = mongoDbName;
+		}
+
+		String mongoUser = System.getenv("MONGO_USER");
+
+		String mongoPassword = System.getenv("MONGO_PASSWORD");
+
+		// If MONGO_MANUAL is set to true, it will set up the DB connection
+		// right away
+		if (isManual) {
+			if (!mongoUser.equals("")) {
+				MongoCredential credential = MongoCredential.createCredential(mongoUser, dbname,
+						mongoPassword.toCharArray());
+				mongoClient = new MongoClient(new ServerAddress(hostname, port),Arrays.asList(credential));
+			}else {
+				mongoClient = new MongoClient(hostname, port);
+			}
+		} else {
+			try {
+				// Check if VCAP_SERVICES exist, and if it does, look up the url
+				// from the credentials.
+				String vcapJSONString = System.getenv("VCAP_SERVICES");
+				if (vcapJSONString != null) {
+					logger.info("Reading VCAP_SERVICES");
+					Object jsonObject = JSONValue.parse(vcapJSONString);
+					JSONObject vcapServices = (JSONObject) jsonObject;
+					JSONArray mongoServiceArray = null;
+					for (Object key : vcapServices.keySet()) {
+						if (key.toString().startsWith("user-provided")) {
+							mongoServiceArray = (JSONArray) vcapServices.get(key);
+							logger.info("Service Type : MongoDB by Compost - " + key.toString());
+							break;
+						}
+					}
+					JSONObject mongoService = (JSONObject) mongoServiceArray.get(0);
+					JSONObject credentials = (JSONObject) mongoService.get("credentials");
+					String url = (String) credentials.get("url");
+					logger.fine("service url = " + url);
+					MongoClientURI mongoURI = new MongoClientURI(url);
+					mongoClient = new MongoClient(mongoURI);
+					dbname = mongoURI.getDatabase();
+
+				}else {
+					mongoClient = new MongoClient(hostname, port);
+				}
+			} catch (Exception e) {
+				logger.severe("Caught Exception : " + e.getMessage());
+			}
+			db = mongoClient.getDatabase(dbname);
+			logger.info("#### Mongo DB Server " + mongoClient.getAddress().getHost() + " ####");
+			logger.info("#### Mongo DB Port " + mongoClient.getAddress().getPort() + " ####");
+			logger.info("#### Mongo DB is created with DB name " + dbname + " ####");
+		}
+
+	}
+
+	public MongoDatabase getDB() {
+		return db;
+	}
+}
