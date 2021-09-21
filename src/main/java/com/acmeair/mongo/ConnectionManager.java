53a20cf68dfdaf59a9ae1f3654055f43e83501cf
Adalberto Junior <adalrsjr1@gmail.com>
2021-09-21 10:09:40 -0400

    add envvars to configure Mongo and Openliberty and update k8s deployment
    ignore intellij files
    
    exposing more Mongo variables through envvars
    
    expose openliberty config through envvars
    
    add log vars
    
    deployment fixes for local kubernetes deployement

--- src/main/java/com/acmeair/mongo/ConnectionManager.java
+++ src/main/java/com/acmeair/mongo/ConnectionManager.java
@@ -4,13 +4,18 @@ import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
+import com.mongodb.connection.ConnectionPoolSettings;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
+import com.acmeair.mongo.MongoConstants;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientOptions.Builder;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 
@@ -41,9 +46,27 @@ public class ConnectionManager implements MongoConstants {
 		String hostname = "localhost";
 		int port = 27017;
 		String dbname = "acmeair";
+		int maxConnections = 100;	// default is 100
+		int minConnections = 10; 	// default is 10
+		int connectionTimeout = 10;	// default is 10
 
 		String mongoManual = System.getenv("MONGO_MANUAL");
 		Boolean isManual = Boolean.parseBoolean(mongoManual);
+
+		String mongoMaxConnectionsPerHost = System.getenv("MONGO_MAX_CONNECTIONS");
+		if (mongoMaxConnectionsPerHost != null) {
+			maxConnections = Integer.parseInt(mongoMaxConnectionsPerHost);
+		}
+
+		String mongoMinConnectionsPerHost = System.getenv("MONGO_MIN_CONNECTIONS");
+		if (mongoMinConnectionsPerHost != null) {
+			minConnections = Integer.parseInt(mongoMinConnectionsPerHost);
+		}
+
+		String mongoConnectionTimeout = System.getenv("MONGO_CONNECTION_TIMEOUT");
+		if (mongoConnectionTimeout != null) {
+			connectionTimeout = Integer.parseInt(mongoConnectionTimeout);
+		}
 
 		String mongoHost = System.getenv("MONGO_HOST");
 		if (mongoHost != null) {
@@ -63,6 +86,13 @@ public class ConnectionManager implements MongoConstants {
 		String mongoUser = System.getenv("MONGO_USER");
 
 		String mongoPassword = System.getenv("MONGO_PASSWORD");
+		MongoClientOptions.Builder clientOptions = new MongoClientOptions.Builder();
+		clientOptions.connectionsPerHost(maxConnections)
+				.minConnectionsPerHost(minConnections)
+				.connectTimeout(connectionTimeout);
+
+		MongoClientOptions options = clientOptions.build();
+
 		try {
 
 			// If MONGO_MANUAL is set to true, it will set up the DB connection
@@ -71,9 +101,9 @@ public class ConnectionManager implements MongoConstants {
 				if (mongoUser != null) {
 					MongoCredential credential = MongoCredential.createCredential(mongoUser, dbname,
 							mongoPassword.toCharArray());
-					mongoClient = new MongoClient(new ServerAddress(hostname, port),Arrays.asList(credential));
+					mongoClient = new MongoClient(new ServerAddress(hostname, port),Arrays.asList(credential),options);
 				}else {
-					mongoClient = new MongoClient(hostname, port);
+					mongoClient = new MongoClient(new ServerAddress(hostname, port), options);
 				}
 			} else {
 				// Check if VCAP_SERVICES exist, and if it does, look up the url
@@ -95,12 +125,12 @@ public class ConnectionManager implements MongoConstants {
 					JSONObject credentials = (JSONObject) mongoService.get("credentials");
 					String url = (String) credentials.get("url");
 					logger.fine("service url = " + url);
-					MongoClientURI mongoURI = new MongoClientURI(url);
+					MongoClientURI mongoURI = new MongoClientURI(url, clientOptions);
 					mongoClient = new MongoClient(mongoURI);
 					dbname = mongoURI.getDatabase();
 
 				}else {
-					mongoClient = new MongoClient(hostname, port);
+					mongoClient = new MongoClient(new ServerAddress(hostname, port), options);
 				}
 			}
 			logger.fine("#### Mongo DB Database Name " + dbname + " ####");
@@ -108,6 +138,9 @@ public class ConnectionManager implements MongoConstants {
 			logger.info("#### Mongo DB Server " + mongoClient.getAddress().getHost() + " ####");
 			logger.info("#### Mongo DB Port " + mongoClient.getAddress().getPort() + " ####");
 			logger.info("#### Mongo DB is created with DB name " + dbname + " ####");
+			logger.info("#### Mongo Max Conn " + maxConnections + " ####");
+			logger.info("### Mongo Configuration:");
+			logger.info(options.toString());
 		} catch (Exception e) {
 			logger.severe("Caught Exception : " + e.getMessage());
 		}
