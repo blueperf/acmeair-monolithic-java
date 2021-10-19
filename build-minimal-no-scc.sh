9a25fd619c244dffed25e569fa988e7fa6b6efe1
jdmcclur <jdmcclur@us.ibm.com>
2021-10-19 10:44:12 -0500

    add no-scc option example

---
+++ build-minimal-no-scc.sh
@@ -0,0 +1,2 @@
+docker build -t acmeair-mono-base-minimal-no-scc -f Dockerfile-base-minimal-no-scc --build-arg CONFIG_DIRECTORY=src/main/liberty/config --build-arg WAR_FILE=target/acmeair-java-2.0.0-SNAPSHOT.war .
+docker build -t acmeair-mono-app-minimal-no-scc  -f Dockerfile-app-minimal-no-scc  --build-arg CONFIG_DIRECTORY=src/main/liberty/config --build-arg WAR_FILE=target/acmeair-java-2.0.0-SNAPSHOT.war .
