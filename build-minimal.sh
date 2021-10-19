7aef40249c914219e38dd181cfc1518a3fe246a0
jdmcclur <jdmcclur@us.ibm.com>
2021-10-18 19:33:51 -0500

    Use populate_scc.sh to add scc layers. Also fix some permissions and cleanup code

--- build-minimal.sh
+++ build-minimal.sh
@@ -1,2 +1,2 @@
-docker build -t acmeair-mono-base-minimal -f Dockerfile-base-minimal --build-arg WAR_FILE=target/acmeair-java-2.0.0-SNAPSHOT.war .
-docker build -t acmeair-mono-app-minimal  -f Dockerfile-app-minimal --build-arg WAR_FILE=target/acmeair-java-2.0.0-SNAPSHOT.war .
+docker build -t acmeair-mono-base-minimal -f Dockerfile-base-minimal --build-arg CONFIG_DIRECTORY=src/main/liberty/config --build-arg WAR_FILE=target/acmeair-java-2.0.0-SNAPSHOT.war .
+docker build -t acmeair-mono-app-minimal  -f Dockerfile-app-minimal --build-arg CONFIG_DIRECTORY=src/main/liberty/config --build-arg WAR_FILE=target/acmeair-java-2.0.0-SNAPSHOT.war .
